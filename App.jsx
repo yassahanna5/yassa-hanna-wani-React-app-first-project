8eb179b544be9d1423a394203258d9d6fbc74cda
yassahanna5 <yassahanna5@gmail.com>
2025-08-31 15:28:11 +0300

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { useState } from 'react'
+import Home from './Home.jsx'
+import About from './About.jsx'
+import Contact from './Contact.jsx'
+
+function App() {
+  let [count, setCount] = useState(0)
+
+  
+
+  return (
+    
+      
+      <div>
+        <button onClick={() => setCount((count) => count +1)}>
+          count increment {count}
+        </button>
+        <br></br>
+        <br></br>
+        <button onClick={() => setCount((count) => count -1)}>
+          count decrement {count}
+        </button>
+        
+        <Home />
+        <About />
+        <Contact />
+      </div>
+    
+  )
+}
+export default App;
+
+
